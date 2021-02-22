09b0a6d81022577f438efba5d592d6debe083fb9
Jeff Fields <jkfields@yahoo.com>
2021-02-22 14:11:57 -0500

    Update appliance.py

--- appliance.py
+++ appliance.py
@@ -5,32 +5,32 @@
 import urllib2
 import json
 
-HTTP_AUTH = 201
-HTTP_NOAUTH = 401
+HTTP_OK = 200
+REQ_CREATED = 201
+REQ_NOAUTH = 401
 
 class StorageAppliance:   
     def __init__(self, host):
         self.host = host
-        self.base_uri = 'https://{host}:215'.format(host=host)
-        
+        self.base_uri = 'https://{host}:215'.format(host=self.host)
         self.opener = urllib2.build_opener(urllib2.HTTPhandler)
         
-        req = urllib2.Request('{uri}/api/access/v1'.format(uri=self.base_uri))
-        req.add_header("X-Auth-User", StorageAppliance.get_user())
-        req.add_header("X-Auth-Key", StorageAppliance.get_pass())
-        
         try:
-            resp = urllib2.urlopen(req)
+            req = urllib2.Request('{uri}/api/access/v1'.format(uri=self.base_uri))
+            req.add_header("X-Auth-User", StorageAppliance.get_user())
+            req.add_header("X-Auth-Key", StorageAppliance.get_pass())
             
+            resp = urllib2.urlopen(req)
             rtncode = resp.getcode()
-            if rtncode == HTTP_AUTH:
+            if rtncode == REQ_CREATED:
+                self.session = resp.info().getheader('X-Auth-Session')
                 self.headers = [ ('Accept', 'application/json'),
                                   ('Content-Type', 'application/json'),
-                                 ('X-Auth-Session', resp.info().getheader('X-Auth-Session')) 
+                                 ('X-Auth-Session', self.session) 
                                ]
                 self.opener.addheaders(self.headers)
             else:
-                msg = 'Authorization failed:  code {code}'
+                msg = 'Failed to create the session; authorization failed: code {code}'
                 raise SystemExit(msg.format(rtncode))
             
         except urllib2.HTTPError as err:
